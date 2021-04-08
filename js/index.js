6f19c4ae44e76a9e95cdd703e68833a19869efd8
Grigoriy Demidovich <grig.demidovich@gmail.com>
2021-04-09 01:18:29 +0300

    wip

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
 // button.disabled = true
 // button.disabled = false
 
-const colors = ["#FFFFFF", "#2196F3", "#4CAF50", "#FF9800", "#009688", "#795548"]
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
 
 const randomIntegerFromInterval = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1) + min)
-}
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 const refs = {
-	startBtn: document.querySelector('button[data-action="start"]'),
-}
+  startBtn: document.querySelector('button[data-action="start"]'),
+  body: document.getElementsByTagName('body')[0],
+};
 
-refs.startBtn.addEventListener("click", () => {
-    console.log(randomIntegerFromInterval(0, 5));
-})
+refs.startBtn.addEventListener('click', () => {
+	console.log(randomIntegerFromInterval(0, 5));
+	console.log(refs.body);
+  refs.body.style.backgroundColor = '#2196F3';
+});
 
 ////////
 // Есть массив цветов в hex-формате и кнопки Start и Stop
@@ -21,4 +31,3 @@ refs.startBtn.addEventListener("click", () => {
 // При нажатии на кнопку Stop, изменение цвета фона должно останавливаться.
 // Учти, на кнопку Start можно нажать бесконечное количество раз. Сделай так, чтобы пока изменение темы запушено, кнопка Start была не активна
 // Для генерации случайного числа (индекс элемента массива цветов), используй функцию randomIntegerFromInterval
-
